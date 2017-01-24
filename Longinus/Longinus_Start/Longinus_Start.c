3855325302074d73912124d54ec2660d5fe6de5a
Darkness-TX <1545492547@qq.com>
2017-01-24 10:48:23 +0800

    启动程序更新1.0.0.2版

--- Longinus/Longinus_Start/Longinus_Start.c
+++ Longinus/Longinus_Start/Longinus_Start.c
@@ -14,6 +14,7 @@ typedef unsigned char  unit8;
 typedef unsigned short unit16;
 typedef unsigned int   unit32;
 
+//想了想，还是用unicode吧......
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,  LPWSTR lpCmdLine,  int nShowCmd)
 {
 	UNREFERENCED_PARAMETER(hInstance);
@@ -27,6 +28,7 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,  LPWSTR lpCm
 	wchar_t iniPath[MAX_PATH];
 	wchar_t exePath[MAX_PATH];
 	wchar_t dllPath[MAX_PATH];
+	unit8 dllPathA[MAX_PATH];
 	GetCurrentDirectoryW(MAX_PATH, dirPath);
 	wsprintfW(iniPath, L"%ls\\%ls", dirPath, L"Longinus.ini");
 	if (_waccess(iniPath, 4) == -1)
@@ -34,9 +36,20 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,  LPWSTR lpCm
 		MessageBoxW(NULL, L"Longinus.ini文件不存在！", L"错误", MB_OK);
 		exit(0);
 	}
-	GetPrivateProfileStringW(L"FileName", L"DLL", L"", dllPath, MAX_PATH, iniPath);
+	if (GetPrivateProfileStringW(L"FileName", L"DLL", L"", dllPath, MAX_PATH, iniPath) == 0)
+	{
+		MessageBoxW(NULL, L"启动失败！请检查DLL属性是否正确！", L"错误", MB_OK);
+		exit(0);
+	}
 	GetPrivateProfileStringW(L"FileName", L"EXE", L"", exePath, MAX_PATH, iniPath);
-	DetourCreateProcessWithDllW(NULL, exePath, NULL, NULL, FALSE, CREATE_DEFAULT_ERROR_MODE, NULL, dirPath, &si, &pi, "Longinus.dll", NULL);
+	WideCharToMultiByte(CP_ACP, 0, dllPath, -1, dllPathA, MAX_PATH, NULL, NULL);
+	if (wcschr(exePath, L'\\') != NULL)
+	{
+		MessageBoxW(NULL, L"EXE属性不支持带目录形式，\n请确保EXE与启动程序在同一个目录下！", L"错误", MB_OK);
+		exit(0);
+	}
+	if (!DetourCreateProcessWithDllW(NULL, exePath, NULL, NULL, FALSE, CREATE_DEFAULT_ERROR_MODE, NULL, dirPath, &si, &pi, dllPathA, NULL))
+		MessageBoxW(NULL, L"启动失败！请检查EXE属性是否正确！", L"错误", MB_OK);
 	WaitForSingleObject(pi.hProcess, INFINITE);
 	CloseHandle(&si);
 	CloseHandle(&pi);
