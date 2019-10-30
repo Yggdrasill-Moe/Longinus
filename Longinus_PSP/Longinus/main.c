e5d6035d2793762cc52bb0fc054fb29686a456bf
Darkness-TX <1545492547@qq.com>
2019-10-30 08:24:18 +0800

    导出成功，加入导入ITV代码

--- Longinus_PSP/Longinus/main.c
+++ Longinus_PSP/Longinus/main.c
@@ -17,7 +17,7 @@ PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 unit32 GetFileName_add = 0;
 unit32 GetStruct_add = 0;
 unit32 GetFile_add = 0;
-unit32 Isfirst = 1;
+//unit32 Isfirst = 1;
 
 unit16 Width = 0;
 unit16 Height = 0;
@@ -25,8 +25,8 @@ unit32 Frame = 0;
 unit32 FrameNum = 0;
 unit32 BitAddress = 0;
 unit8 ITVName[32] = { 0 };
-unit8 dstName[64] = { 0 };
-unit32 pal[256] = { 0 };
+unit8 dstName[128] = { 0 };
+//unit32 pal[256] = { 0 };
 
 /*//不知道是不是libpng库太老了只要png_write_info就崩溃，所以注释掉
 void WritePng(FILE *fp,unit32 width,unit32 height,unit32 *pal,unit8 *data)
@@ -60,7 +60,7 @@ void WritePng(FILE *fp,unit32 width,unit32 height,unit32 *pal,unit8 *data)
 	free(png_alpha);
 }
 */
-
+/*
 void CheckITV()
 {
 	if (Isfirst)
@@ -68,7 +68,7 @@ void CheckITV()
 		if (GetFileName_add != 0)
 		{
 			strcpy(ITVName,(unit8 *)GetFileName_add);
-			GetFileName_add = 0; 
+			GetFileName_add = 0;
 			sprintf(dstName,"ms0:/%s",ITVName);
 			sceIoMkdir(dstName,0777);
 			FrameNum = *(unit32 *)(GetStruct_add + 0x8);
@@ -108,7 +108,34 @@ void CheckITV()
 			Isfirst = 1;
 	}
 }
+*/
+void BuildInfo()
+{
+	if (GetFileName_add != 0)
+	{
+		strcpy(ITVName,(unit8 *)GetFileName_add);
+		GetFileName_add = 0;
+		FrameNum = *(unit32 *)(GetStruct_add + 0x8);
+		Width = *(unit16 *)(GetStruct_add + 0x16);
+		Height = *(unit16 *)(GetStruct_add + 0x18);
+		BitAddress = *(unit32 *)(GetStruct_add + 0x1C);
+		GetFile_add = *(unit32 *)(GetStruct_add + 0x3C);
+		sprintf(dstName,"disc0:/PSP_GAME/USRDIR/mmv3/%s/pal.bin",ITVName);
+		SceUID fd = sceIoOpen(dstName, PSP_O_RDONLY, 0777);
+		sceIoRead(fd,(unit8 *)(GetFile_add + 0x1E),0x100 * 4);
+		sceIoClose(fd);
+	}
+}
 
+void ImportFrame()
+{
+	Frame = *(unit32 *)(GetStruct_add + 0x48);
+	sprintf(dstName,"disc0:/PSP_GAME/USRDIR/mmv3/%s/%03d.bin",ITVName,Frame);
+	SceUID fd = sceIoOpen(dstName, PSP_O_RDONLY, 0777);
+	sceIoRead(fd,(unit8 *)BitAddress,Width * Height);
+	sceIoClose(fd);
+}
+/*
 void GetFileName()
 {
 	//这里只是sp加0x10后正好是文件名所在地址而已，没有特别的含义
@@ -133,18 +160,46 @@ void GetStruct()
 	asm("addiu $sp,$sp,0x4");
 	asm("jr $ra");
 }
+*/
+void GetInfo()
+{
+	asm("addiu $sp,$sp,0x10");
+	asm("sw $sp,GetFileName_add");
+	asm("sw $s0,GetStruct_add");
+	asm("addiu $sp,$sp,-0x10");
+	//由于这里使用了jal，所以要开个栈保存ra跳转的地址
+	asm("addiu $sp,$sp,-0x4");
+	asm("sw $ra,0x0($sp)");
+	asm("jal BuildInfo");
+	asm("lw $ra,0x0($sp)");
+	asm("addiu $sp,$sp,0x4");
+	asm("jr $ra");
+}
+
+void ImportITV()
+{
+	//由于这里使用了jal，所以要开个栈保存ra跳转的地址
+	asm("addiu $sp,$sp,-0x4");
+	asm("sw $ra,0x0($sp)");
+	asm("jal ImportFrame");
+	asm("lw $ra,0x0($sp)");
+	asm("addiu $sp,$sp,0x4");
+	asm("jr $ra");
+}
 
 int module_start(int argc, char *argv[])
 {
 	//内存中的hook地址，为了方便选择了函数最后的jr ra返回指令覆盖成j xxxxxxxx，跳转后一般已经还原了堆栈
 	unit32 hook_GetFileName = 0x0897A1DC;
 	// j xxxxxxxx，MIPS指令是4字节对齐，所以j xxxxxxxx为内存地址右移两位后或j的opcode
-	*(unit32 *)(hook_GetFileName) = 0x08000000 | (((unit32)GetFileName) & 0x0FFFFFFC) >> 2;
+	//*(unit32 *)(hook_GetFileName) = 0x08000000 | (((unit32)GetFileName) & 0x0FFFFFFC) >> 2;
+	*(unit32 *)(hook_GetFileName) = 0x08000000 | (((unsigned int)GetInfo) & 0x0FFFFFFC) >> 2;
 	//最后需要刷新下内存
 	sceKernelDcacheWritebackInvalidateRange((const void *)hook_GetFileName, 4);
 	sceKernelIcacheInvalidateRange((const void *)hook_GetFileName, 4);
 	unit32 hook_GetStruct = 0x0897A020;
-	*(unit32 *)(hook_GetStruct) = 0x08000000 | (((unit32)GetStruct) & 0x0FFFFFFC) >> 2;
+	//*(unit32 *)(hook_GetStruct) = 0x08000000 | (((unit32)GetStruct) & 0x0FFFFFFC) >> 2;
+	*(unit32 *)(hook_GetStruct) = 0x08000000 | (((unsigned int)ImportITV) & 0x0FFFFFFC) >> 2;
 	sceKernelDcacheWritebackInvalidateRange((const void *)hook_GetStruct, 4);
 	sceKernelIcacheInvalidateRange((const void *)hook_GetStruct, 4);
 	return 0;
